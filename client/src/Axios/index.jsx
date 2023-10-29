d269e9af47c19477fc500d89bfe23d5b3d5286d6
Md <shoyebmd424@gmail.com>
2023-10-29 18:07:27 +0530

    readme

--- client/src/Axios/index.jsx
+++ client/src/Axios/index.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 // const Axios = axios.create({ baseURL: "http://localhost:8888/book" });
-const Axios = axios.create({ baseURL: "https://backend-challenge-e5c8.onrender.com/" });
+const Axios = axios.create({
+  baseURL: "https://backend-challenge-e5c8.onrender.com/book",
+});
 
 export default Axios;
